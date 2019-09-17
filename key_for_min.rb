7eddafd0c25a404fe65f6705b5221a6c3efa5e50
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-17 23:45:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,34 @@
-require "pry"
-
 def key_for_min_value(name_hash)
-  name_hash.each do |name, ass_num|
-    
+  
+  # GET ALL NUMBER VALUES 
+  
+  num_vals = name_hash.map { | name, num | num }
+
+  # GET MAX NUMBER FIRST
+  
+  max_num = 0
+  num_vals.each do |num|
+    if num > max_num
+      max_num = num
+    end
   end
+
+  # SET MIN NUMBER TO MAX NUMBER FROM ^ ABOVE
+  
+  min_num = max_num
+  num_vals.each do |num|
+    if num < min_num
+      min_num = num
+    end
+  end
+  
+  # SET PERSON TO NAME OF PERSON WITH SMALLEST NUMBER VALUE 
+  
+  person = ""
+  name_hash.each do |name, number|
+    if number == min_num
+      person = name
+    end
+  end
+  person
 end
