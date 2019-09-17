ae86f72c8dd11c6a2965a01f606f3882fe37adcb
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-17 23:12:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,16 +1,5 @@
 require "pry"
 
 def key_for_min_value(name_hash)
-  greatest_num = 0
-  arrange_least_to_greatest = []
-  name_hash.each do |name, value|
-    if value > greatest_num
-      greatest_num = value
-      arrange_least_to_greatest << name
-    else 
-      arrange_least_to_greatest << name
-    end
-    # binding.pry
-  end
-  arrange_least_to_greatest[0]
+  name_hash.each do |name, |
 end
