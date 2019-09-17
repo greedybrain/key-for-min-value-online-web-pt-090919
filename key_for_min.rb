d837619358b762ac2fa6d4e36efd0b6fa2e7e9f0
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-17 22:22:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,16 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+require "pry"
 
 def key_for_min_value(name_hash)
-
+  greatest_num = 0
+  arrange_least_to_greatest = []
+  name_hash.each do |name, value|
+    if value > greatest_num
+      greatest_num = value
+      arrange_least_to_greatest << name
+    else 
+      arrange_least_to_greatest << name
+    end
+    # binding.pry
+  end
+  arrange_least_to_greatest[0]
 end
